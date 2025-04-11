b7c96c608fbddb7ac5139ff80062144578882aff
ChaseRensberger <chasehrensberger@gmail.com>
2025-04-11 15:05:47 -0400

    add vite react hero

---
+++ website/src/App.tsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <main className="flex flex-col text-center items-center px-12 py-24 min-h-screen gap-4">
+      <h1 className="text-4xl font-bold">Wingman</h1>
+      <h2 className="text-2xl text-gray-500">The open source operating system for tool assisted voice agents</h2>
+    </main>
+  )
+}
