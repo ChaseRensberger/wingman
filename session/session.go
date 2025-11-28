67593f266eab1931810afe141c5c3144a951bd75
Chase Rensberger <chasehrensberger@gmail.com>
2025-11-28 17:04:58 -0500

    checkpoint

---
+++ session/session.go
@@ -0,0 +1,26 @@
+package session
+
+import "context"
+
+type Session struct {
+	inferenceProvider InferenceProvider
+	history           []any
+}
+
+func CreateSession(inferenceProvider InferenceProvider) *Session {
+	return &Session{
+		inferenceProvider: inferenceProvider,
+		history:           []any{},
+	}
+}
+
+func (s *Session) RunInference(ctx context.Context, input any) (any, error) {
+	result, err := s.inferenceProvider.RunInference(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	s.history = append(s.history, input, result)
+
+	return result, nil
+}
