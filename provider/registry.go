d6338c8bcf4f42fd140efc4faf0da9e2be0c5421
Chase Rensberger <chasehrensberger@gmail.com>
2025-11-28 17:10:16 -0500

    add registry

--- provider/registry.go
+++ provider/registry.go
@@ -1,1 +1,15 @@
 package provider
+
+type ProviderRegistry struct {
+	providers map[string]InferenceProvider
+}
+
+func NewHandlerRegistry() *ProviderRegistry {
+	return &ProviderRegistry{
+		providers: make(map[string]InferenceProvider),
+	}
+}
+
+func (r *ProviderRegistry) Register(name string, provider InferenceProvider) {
+	r.providers[name] = provider
+}
