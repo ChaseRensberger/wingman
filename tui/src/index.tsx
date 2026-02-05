0be9a3aad8a23bbd2c4a1b7be0cc022d10cb2543
Chase Rensberger <chasehrensberger@gmail.com>
2026-02-05 18:02:09 -0500

    checkpoint

--- tui/src/index.tsx
+++ tui/src/index.tsx
@@ -1,20 +1,22 @@
 import { createCliRenderer } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 import { useState } from "react";
+import { logo } from "./logo";
 
 function App() {
 	const [value, setValue] = useState("");
 
 	return (
-		<box flexDirection="column" flexGrow={1} padding={1} backgroundColor="#000000">
-			<box height={4} backgroundColor="#428f84" />
-			<box flexGrow={1} backgroundColor="#42cf84" />
-			{/* Input Box*/}
+		<box flexDirection="column" flexGrow={1} padding={1}>
+			<box height={4} backgroundColor="#2a2a2a" />
+			<box flexGrow={1} justifyContent="center" alignItems="center" borderColor="#FFFFFF">
+				<text fg="#0088ff">{logo}</text>
+			</box>
 			<box height={4} backgroundColor="#2a2a2a" padding={1}>
 				<input
 					value={value}
 					onChange={setValue}
-					placeholder="Type a message..."
+					placeholder="Ask anything..."
 					focused
 					flexGrow={1}
 					textColor="#ffffff"
