83754bb4e86a539a9f3cdf9e958aa2f17888a428
Chase Rensberger <chasehrensberger@gmail.com>
2026-02-06 16:47:32 -0500

    make hero header sticky

--- web/apps/hero/client/src/App.tsx
+++ web/apps/hero/client/src/App.tsx
@@ -55,7 +55,7 @@ function NavLink(navItem: {
 export default function App() {
 	return (
 		<main className="min-h-screen flex flex-col md:max-w-2xl lg:max-w-4xl mx-auto border">
-			<nav className="flex items-center justify-between px-6 py-2 w-full border-b">
+			<nav className="sticky top-0 bg-background flex items-center justify-between px-6 py-2 w-full border-b">
 				<img src={WingmanIcon} className="w-12 h-12" />
 				<div className="flex items-center gap-6">
 					<NavLink name="GitHub" url={GITHUB_URL} />
@@ -71,7 +71,6 @@ export default function App() {
 					<CopyCommand />
 				</div>
 			</section>
-
 			<footer className="px-6 py-2 text-center">
 				<p className="text-sm text-muted-foreground font-mono">
 					Hero
