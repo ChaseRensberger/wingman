5a4c12f5de7d3594db285523a573512bb99ae785
Chase Rensberger <chasehrensberger@gmail.com>
2026-02-06 17:03:05 -0500

    fix workspace/ui import

--- web/apps/hero/vite.config.ts
+++ web/apps/hero/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@workspace/ui": path.resolve(__dirname, "../../../packages/core/src"),
+      "@workspace/ui": path.resolve(__dirname, "../../packages/core/src"),
     },
   },
 });
