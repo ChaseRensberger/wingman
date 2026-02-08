4653d70c1c6be81319044f3b9af6e82ac189c4e8
Chase Rensberger <chasehrensberger@gmail.com>
2026-02-08 14:32:48 -0500

    system theme

--- web/apps/hero/src/routes/__root.tsx
+++ web/apps/hero/src/routes/__root.tsx
@@ -1,8 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { ThemeProvider } from '@wingman/core/components/theme-provider'
 
 const RootLayout = () => (
 	<>
-		<Outlet />
+		<ThemeProvider defaultTheme='system' storageKey='wingman-ui-theme'>
+			<Outlet />
+		</ThemeProvider>
 	</>
 )
 
