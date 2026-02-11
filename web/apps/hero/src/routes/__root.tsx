7e7b7686b8a3b45024559487564999aecea4fa36
Chase Rensberger <chasehrensberger@gmail.com>
2026-02-11 18:44:02 -0500

    vercel analytics

--- web/apps/hero/src/routes/__root.tsx
+++ web/apps/hero/src/routes/__root.tsx
@@ -1,8 +1,10 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { ThemeProvider } from '@wingman/core/components/theme-provider'
+import { Analytics } from '@vercel/analytics/next'
 
 const RootLayout = () => (
 	<>
+		<Analytics />
 		<ThemeProvider defaultTheme='system' storageKey='wingman-ui-theme'>
 			<Outlet />
 		</ThemeProvider>
