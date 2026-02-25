7dacda2730716dbbac0448c7901724e10ec89d26
Chase Rensberger <chasehrensberger@gmail.com>
2026-02-25 14:41:03 -0500

    remove docs link

--- clients/wui/hero/src/routes/index.tsx
+++ clients/wui/hero/src/routes/index.tsx
@@ -88,9 +88,9 @@ function Hero() {
 				<img src={WingmanIcon} className="w-12 h-12" />
 				<div className="flex items-center gap-6">
 					<NavLink name="GitHub" url={GITHUB_URL} />
-					<NavLink name="Docs" url={"/docs"} />
 					{/*
 					<NavLink name="Blog" url={"/blog"} />
+					<NavLink name="Docs" url={"/docs"} />
 					*/}
 				</div>
 			</nav>
