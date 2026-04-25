ac052f18381df861e8921cb954a9e68c8eae329a
Chase Rensberger <chasehrensberger@gmail.com>
2026-04-25 19:22:07 -0400

    move hero

---
+++ hero/src/vite-env.d.ts
@@ -0,0 +1,20 @@
+declare module "virtual:docs" {
+  export interface Doc {
+    slug: string;
+    title: string;
+    group: string;
+    order: number;
+    content: string;
+  }
+  export const docs: Doc[];
+}
+
+declare module "virtual:blog" {
+  export interface Post {
+    slug: string;
+    title: string;
+    date: string;
+    content: string;
+  }
+  export const posts: Post[];
+}
