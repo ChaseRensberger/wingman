8c8a3bfad82655758517329e203f76ff5ff89639
Chase Rensberger <chasehrensberger@gmail.com>
2026-06-13 01:30:03 -0400

    router basepath

--- web/apps/console/src/router.tsx
+++ web/apps/console/src/router.tsx
@@ -1,7 +1,7 @@
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export const router = createRouter({ routeTree, basepath: "/web" });
+export const router = createRouter({ routeTree, basepath: "/console" });
 
 declare module "@tanstack/react-router" {
   interface Register {
